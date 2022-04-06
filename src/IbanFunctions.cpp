ef3e333f39ed038ef996bbe9c6b37a9529f38425
dtrommel <80407337+dtrommel@users.noreply.github.com>
2022-04-06 15:11:42 +0200

    Implemented multi threading fully.

--- src/IbanFunctions.cpp
+++ src/IbanFunctions.cpp
@@ -3,6 +3,7 @@
 //
 #include <thread>
 #include <mutex>
+#include <future>
 
 using namespace std::chrono;
 std::mutex mutex;
@@ -27,11 +28,11 @@ void Count (int b, int e, int m){
 
 
 void CountMode(int b, int e, int m, int p){
-    //Time was 5.46338 without concurrent optimization
-    //Time was 1.37416 with 8 threads enabled
-    //Time was 1.25386 with 16 threads enabled
-    //Time was 1.25151 with 32 threads enabled
-    //Time was 1.2441 with 64 threads enabled
+    //Time was 5.46338 without concurrent optimization Macbook M1 Pro
+    //Time was 1.37416 with 8 threads enabled Macbook M1 Pro
+    //Time was 1.25386 with 16 threads enabled Macbook M1 Pro
+    //Time was 1.25151 with 32 threads enabled Macbook M1 Pro
+    //Time was 1.2441 with 64 threads enabled Macbook M1 Pro
 
     //Starting clock for performance purposes
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
@@ -77,37 +78,95 @@ void CountMode(int b, int e, int m, int p){
 
 }
 
+static int indexNumber = 0;
+/// Functie om gevonden getallen naar de console te schrijven
+/// \param b Begin van het bereik (inclusief)
+/// \param e Eind van het bereik (exclusief)
+/// \param m Modulus
+void List(int b, int e, int m){
+    for (int i = b;i < e;i++){
+        if(mCheck(i,m)){
+            mutex.lock();
+            std::cout<< indexNumber << ": " << i << std::endl;
+            indexNumber++;
+            mutex.unlock();
+        }
+    }
+}
+
 void ListMode(int b, int e, int m, int p){
+    int startNumber = b;
+    int threadNumber = p;
+    int numberPerThread = (e-startNumber)/threadNumber;
+    int remaining = (e - startNumber)%threadNumber;
+
     std::cout << "ListMode activated" << std::endl;
-    int number = 1;
-    for (int i = b;i<e;i++){
-        if (mCheck(i,m)) {
-            std::cout << number << " " << i << std::endl;
-            number++;
+    std::vector<std::thread> threads;
+
+    //Create threads to execute functions
+    for (int i = 0; i < threadNumber; i++)
+    {
+        if (remaining != 0){
+            int endNumber = startNumber+numberPerThread+1;
+            threads.push_back(std::thread(List, startNumber, endNumber,m));
+            remaining--;
+            startNumber = endNumber;
+
+        }
+        else{
+            int endNumber = startNumber+numberPerThread;
+            threads.push_back(std::thread(List,startNumber,endNumber,m));
+            startNumber = endNumber;
+        }
+    }
+
+    //Join threads
+    for (int i = 0; i < threads.size(); i++)
+    {
+        threads.at(i).join();
+    }
+}
+static int searchNumber = 0;
+
+int Search(int b, int e, int m, int s){
+    for (int i = b;i < e;i++){
+        if(mCheck(i,m)){
+            if (i==s) return i;
         }
     }
 }
 
 void SearchMode(int b, int e, int m, int p, int s){
-    std::cout << "SearchMode activated" << std::endl;
-    if (s < b || s >= e) {
-        std::cout << "-1" << std::endl;
+    if (!mCheck(s,m)){
+        std::cout<<"Number to search does not meet "<<m<< " check."<<std::endl;
         return;
     }
-    else if (!mCheck(s,m)){
-        std::cout<< "-1" << std::endl;
-    }
-    else {
-        for (int i =b;i<e;i++){
-            if (i == s){
-                std::cout<<i<<std::endl;
-                return;
-            }
+    int startNumber = b;
+    int threadNumber = p;
+    int numberPerThread = (e-startNumber)/threadNumber;
+    int remaining = (e - startNumber)%threadNumber;
+
+    std::cout << "ListMode activated" << std::endl;
+    std::vector<std::thread> threads;
+
+    std::future<int> result;
+    //Create threads to execute functions
+    for (int i = 0; i < threadNumber; i++)
+    {
+        if (remaining != 0){
+            int endNumber = startNumber+numberPerThread+1;
+            result = std::async(std::launch::async, Search,startNumber,endNumber,m,s);
+            remaining--;
+            startNumber = endNumber;
+
+        }
+        else{
+            int endNumber = startNumber+numberPerThread;
+            result = std::async(std::launch::async, Search,startNumber,endNumber,m,s);
+            startNumber = endNumber;
         }
     }
-    std::cout << "-1" << std::endl;
-    return;
-
+    std::cout<<result.get()<<std::endl;
 }
 
 
